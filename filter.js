583b2a2ccd2dd215fec8b7a4f3e45e202d455228
Cecilia Swahn Gleisner <cecilia@lifeistooshort.se>
2024-03-05 08:26:54 +0000

    Part two

--- filter.js
+++ filter.js
@@ -65,4 +65,9 @@ const students = [
   },
 ];
 
+//Sometimes it's easier to read if you start by writing your call back function
 
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log('candidates:',candidates);
